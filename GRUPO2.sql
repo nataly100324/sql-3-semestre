eea9c68eaf9fc7c8c3b4d9d979de5d7e543e86a6
nataly100324 <pumitanataly5@gmail.com>
2025-04-01 13:58:45 -0500

    Update GRUPO2.sql

--- GRUPO2.sql
+++ GRUPO2.sql
@@ -23,6 +23,31 @@ orden NUMBER,
 estado char(1)
 );
 
+--yuberli miranda
+--lucia puma
+
+-----------------------------
+
+CREATE TABLE generos (
+id_genero NUMBER PRIMARY KEY,
+nombre_genero varchar2(15) UNIQUE NOT NULL,
+sigla VARCHAR2 (2) UNIQUE,
+orden NUMBER,
+estado char(1)
+);
+
+----------------------------------------
+--TIPO DOCUMEDROP TABLENTO
+CREATE TABLE tipo_documento (
+id_tipo_documento NUMBER PRIMARY KEY,
+nombre  varchar2(150) UNIQUE NOT NULL,
+sigla VARCHAR2 (5) UNIQUE,
+pais VARCHAR2 (50),
+cantidad_digitos NUMBER,
+orden NUMBER,
+estado char(1)
+);
+
 ----------------------------------------
 --TABLA PERSONA
 CREATE TABLE personas(
@@ -48,7 +73,7 @@ REFERENCES generos (id_genero)
 
 );
 ---------------------------------------
-CREATE TABLE clientes (
+CREATE TABLE cliente (
 id_cliente NUMBER PRIMARY KEY,
 id_persona NUMBER UNIQUE,
 direccion VARCHAR2(50),
@@ -65,7 +90,7 @@ REFERENCES personas (id_persona)
 CREATE TABLE personal(
 id_personal NUMBER PRIMARY KEY,
 id_persona NUMBER UNIQUE,
-id_tienda NUMBER UNIQUE,
+id_venta NUMBER ,
 direccion VARCHAR2(50),
 cargo varchar2 (100) NOT NULL,
 sueldo NUMBER (10,2) NOT NULL,
@@ -79,17 +104,16 @@ REFERENCES personas (id_persona)
 
 );
 
-
+--------------------------------
 CREATE TABLE tipo_pago(
 id_tipo_pago NUMBER PRIMARY KEY,
 nombre VARCHAR2 (20),
 estado char(1),
 orden NUMBER
 );
-
+--------------------------
 CREATE TABLE dato_envio_cliente (
-dato_envio_cliente NUMBER PRIMARY KEY,
-id_cliente NUMBER,
+id_dato_envio_cliente NUMBER PRIMARY KEY,
 estado char(1),
 fecha_envio DATE,
 fecha_entrega DATE,
@@ -98,7 +122,7 @@ costo_envio NUMBER (5,2),
 direccion VARCHAR2(50),
 ubicacion VARCHAR2 (30)
 );
-
+-------------------------
 
 CREATE TABLE tienda(
 id_tienda NUMBER PRIMARY KEY,
@@ -108,7 +132,7 @@ telefono NUMBER (9),
 estado char(1)
 
 );
-
+----------------------------------
 CREATE TABLE tipo_comprobante(
 id_tipo_comprobante NUMBER PRIMARY KEY,
 nombre VARCHAR2 (30),
@@ -117,38 +141,61 @@ fecha_emision DATE,
 monto NUMBER (10,2),
 comision NUMBER (5)
 );
-
+-----------------
 CREATE TABLE comprobante_detalle(
 id_comprobante_detalle NUMBER PRIMARY KEY,
 id_cliente NUMBER,
 id_tipo_comprobante NUMBER, 
-id_venta NUMBER,
 id_producto NUMBER,
-id_promocion NUMBER
+id_promocion NUMBER,
+cantidad NUMBER,
+sub_total NUMBER,
+CONSTRAINT fk_tipo_comprobante_comprobante_detalle
+FOREIGN KEY (id_tipo_comprobante)
+REFERENCES tipo_comprobante (id_tipo_comprobante)
 );
 
+------------------------------
 CREATE TABLE carrito_detalle(
 id_carrito_detalle NUMBER PRIMARY KEY,
 id_producto NUMBER (5),
 id_venta NUMBER (5),
-cantidad NUMBER (5)
+cantidad NUMBER (5),
+
+CONSTRAINT fk_venta_carrito_detalle
+FOREIGN KEY (id_venta)
+REFERENCES venta (id_venta),
+
+CONSTRAINT fk_producto_carrito_detalle
+FOREIGN KEY (id_producto)
+REFERENCES producto (id_producto)
 );
 
+------------------------------------------
 CREATE TABLE producto(
 id_producto NUMBER PRIMARY KEY,
 id_proveedor NUMBER,
 id_promocion NUMBER,
 nombre VARCHAR2 (20),
-descripcion VARCHAR2,
+descripcion VARCHAR2(100),
 precio NUMBER (10),
 cantidad NUMBER (5),
 echa_creacion DATE,
-marca VARCHAR2
+marca VARCHAR2 (20),
+
+
+CONSTRAINT fk_proveedor_producto
+FOREIGN KEY (id_proveedor)
+REFERENCES proveedor (id_proveedor),
+
+CONSTRAINT fk_promocion_producto
+FOREIGN KEY (id_promocion)
+REFERENCES promocion (id_promocion)
 );
 
-
+------------------------------------------
 CREATE TABLE proveedor(
-id_proveesor NUMBER PRIMARY KEY,
+id_proveedor NUMBER PRIMARY KEY,
 nombre VARCHAR2 (30),
 telefono NUMBER (9),
 correo VARCHAR2 (20),
@@ -156,6 +203,7 @@ direccion VARCHAR2(50),
 estado char(1)
 );
 
+-------------------------------------------
 CREATE TABLE promocion(
 id_promocion NUMBER PRIMARY KEY,
 descripcion VARCHAR2 (100),
@@ -163,48 +211,138 @@ descuernto NUMBER (20),
 fecha_inicio DATE,
 fecha_fin DATE
 );
-
+--------------------------------------------
 CREATE TABLE reembolso_detalle(
 id_reembolso_detalle NUMBER PRIMARY KEY,
 id_reembolso NUMBER,
-id_carrito_detalle NUMBER
-);
+id_carrito_detalle NUMBER,
 
+CONSTRAINT fk_reembolso_reembolso_detalle
+FOREIGN KEY (id_reembolso)
+REFERENCES reembolso (id_reembolso),
+
+CONSTRAINT fk_carrito_detalle_reembolso_detalle
+FOREIGN KEY (id_carrito_detalle)
+REFERENCES carrito_detalle (id_carrito_detalle)
+
+);
+------------------------------------
 CREATE TABLE reembolso(
 id_reembolso NUMBER PRIMARY KEY,
-id_venta NUMBER UNIQUE,
 id_ticket_soporte NUMBER UNIQUE,
+id_venta NUMBER UNIQUE,
 precio NUMBER,
-fecha_reembolso DATE
-);
+fecha_reembolso DATE,
 
+CONSTRAINT fk_ticket_soporte_reembolso
+FOREIGN KEY (id_ticket_soporte)
+REFERENCES ticket_soporte (id_ticket_soporte),
+
+CONSTRAINT fk_venta_reembolso
+FOREIGN KEY (id_venta)
+REFERENCES venta (id_venta)
+
+);
+--------------------------
 CREATE TABLE ticket_soporte(
 id_ticket_soporte NUMBER PRIMARY KEY,
 id_requisitos NUMBER,
 fecha_creacion DATE,
 descripcion VARCHAR2 (100),
-estado char(1)
-);
+estado char(1),
 
+CONSTRAINT fk_requisitos_ticket_soporte
+FOREIGN KEY (id_requisitos)
+REFERENCES requisitos (id_requisitos)
+
+);
+------------------
 CREATE TABLE requisitos(
 id_requisitos NUMBER PRIMARY KEY,
 descripcion VARCHAR2 (100),
 estado char(1)
 );
 
-
+-------------------------------
 
 CREATE TABLE venta(
 id_venta NUMBER PRIMARY KEY,
 id_cliente NUMBER UNIQUE,
 id_tienda NUMBER ,
 id_tipo_pago NUMBER,
-id_tipo_comprobante NUMBER,
+id_comprobante_detalle NUMBER,
 id_personal NUMBER UNIQUE,
-id_dato_envio NUMBER,
+id_dato_envio_cliente NUMBER,
 fecha_venta DATE,
-estado char(1)
+estado char(1),
+
+CONSTRAINT fk_cliente_venta
+FOREIGN KEY (id_cliente)
+REFERENCES cliente (id_cliente),
+
+CONSTRAINT fk_tienda_venta
+FOREIGN KEY (id_tienda)
+REFERENCES tienda (id_tienda),
+
+CONSTRAINT fk_tipo_pago_venta
+FOREIGN KEY (id_tipo_pago)
+REFERENCES tipo_pago (id_tipo_pago),
+
+CONSTRAINT fk_comprobante_detalle_venta
+FOREIGN KEY (id_comprobante_detalle)
+REFERENCES comprobante_detalle (id_comprobante_detalle),
+
+CONSTRAINT fk_personal_venta
+FOREIGN KEY (id_personal)
+REFERENCES personal (id_personal),
+
+CONSTRAINT fk_dato_envio_cliente_venta
+FOREIGN KEY (id_dato_envio_cliente)
+REFERENCES dato_envio_cliente (id_dato_envio_cliente)
 );
+
+ALTER TABLE venta ADD igb NUMBER(10,2);
+ALTER TABLE venta ADD total NUMBER(10,2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
